fda876c1890951496fac709da3d87a5d7a0cbefb
oliverbb1 <98846468+oliverbb1@users.noreply.github.com>
2025-11-19 19:11:54 +0100

    get products on front

--- front/src/Dashboard.jsx
+++ front/src/Dashboard.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
+import "./Dashboard.css";
+
+import { useState, useEffect } from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/products");
+        // console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, []);
+  return (
+    <div>
+      <h2 className="p-title">Dashboard - Proizvodi</h2>
+      <div className="products-grid">
+        {products.map((p) => (
+          <div key={p._id} className="product-card">
+            <h3>{p.naziv}</h3>
+            <p>Kategorija: {p.kategorija}</p>
+            <p>Cena: {p.cena}</p>
+            <p>Količina: {p.kolicina}</p>
+            <p>Dobavljač: {p.dobavljac}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
